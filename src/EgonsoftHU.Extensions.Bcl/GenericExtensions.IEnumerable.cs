20c4ab0791d753c45292cc5c9ebbc1ea66f2f121
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2026-09-17 00:39:16 +0200

    Add overloads of IsIn() and IsNotIn() extension methods. The values parameter now supports both params IEnumerable<T> and params ReadOnlySpan<T> types.

--- src/EgonsoftHU.Extensions.Bcl/GenericExtensions.IEnumerable.cs
+++ src/EgonsoftHU.Extensions.Bcl/GenericExtensions.IEnumerable.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright © 2022-2026 Gabor Csizmadia
 // This code is licensed under MIT license (see LICENSE for details)
 
+#if NET10_0_OR_GREATER
 using System;
+#endif
 using System.Collections.Generic;
 using System.Linq;
 
@@ -55,18 +57,71 @@ namespace EgonsoftHU.Extensions.Bcl
             return value.IsIn(equalityComparer: null, values);
         }
 
+#if NET9_0_OR_GREATER
         /// <summary>
-        /// Determines whether a specified collection does not contain the current value by using a specified <see cref="IEqualityComparer{T}"/>.
+        /// Determines whether a specified collection contains the current value by using a specified <see cref="IEqualityComparer{T}"/>.
+        /// If no equality comparer is specified then the default equality comparer will be used.
+        /// </summary>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
+        /// <param name="value">The value to locate in the collection.</param>
+        /// <param name="equalityComparer">An equality comparer to compare values.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
+        /// <returns>Returns true if the specified collection contains an element that has the specified value; otherwise, false.</returns>
+        public static bool IsIn<T>(this T? value, IEqualityComparer<T?>? equalityComparer, params IEnumerable<T?> values)
+        {
+            return
+                equalityComparer is null
+                    ? values.Contains(value)
+                    : values.Contains(value, equalityComparer);
+        }
+
+        /// <summary>
+        /// Determines whether a specified collection contains the current value by using a specified <see cref="IEqualityComparer{T}"/>.
         /// If no equality comparer is specified then the default equality comparer will be used.
         /// </summary>
         /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
         /// <param name="value">The value to locate in the collection.</param>
         /// <param name="values">A collection in which to locate a value.</param>
-        /// <returns>Returns true if the specified collection does not contain an element that has the specified value; otherwise, false.</returns>
-        public static bool IsNotIn<T>(this T? value, params T?[] values)
+        /// <returns>Returns true if the specified collection contains an element that has the specified value; otherwise, false.</returns>
+        public static bool IsIn<T>(this T? value, params IEnumerable<T?> values)
         {
-            return !value.IsIn(values);
+            return value.IsIn(equalityComparer: null, values);
         }
+#endif
+
+#if NET10_0_OR_GREATER
+        /// <summary>
+        /// Determines whether a specified collection contains the current value by using a specified <see cref="IEqualityComparer{T}"/>.
+        /// If no equality comparer is specified then the default equality comparer will be used.
+        /// </summary>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
+        /// <param name="value">The value to locate in the collection.</param>
+        /// <param name="equalityComparer">An equality comparer to compare values.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
+        /// <returns>Returns true if the specified collection contains an element that has the specified value; otherwise, false.</returns>
+        public static bool IsIn<T>(this T value, IEqualityComparer<T>? equalityComparer, params ReadOnlySpan<T> values)
+            where T : IEquatable<T>
+        {
+            return
+                equalityComparer is null
+                    ? values.Contains(value)
+                    : values.Contains(value, equalityComparer);
+        }
+
+        /// <summary>
+        /// Determines whether a specified collection contains the current value by using a specified <see cref="IEqualityComparer{T}"/>.
+        /// If no equality comparer is specified then the default equality comparer will be used.
+        /// </summary>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
+        /// <param name="value">The value to locate in the collection.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
+        /// <returns>Returns true if the specified collection contains an element that has the specified value; otherwise, false.</returns>
+        public static bool IsIn<T>(this T value, params ReadOnlySpan<T> values)
+            where T : IEquatable<T>
+        {
+            return value.IsIn(equalityComparer: null, values);
+        }
+#endif
 
         /// <summary>
         /// Determines whether a specified collection does not contain the current value by using the default equality comparer.
@@ -80,5 +135,76 @@ namespace EgonsoftHU.Extensions.Bcl
         {
             return !value.IsIn(equalityComparer, values);
         }
+
+        /// <summary>
+        /// Determines whether a specified collection does not contain the current value by using a specified <see cref="IEqualityComparer{T}"/>.
+        /// If no equality comparer is specified then the default equality comparer will be used.
+        /// </summary>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
+        /// <param name="value">The value to locate in the collection.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
+        /// <returns>Returns true if the specified collection does not contain an element that has the specified value; otherwise, false.</returns>
+        public static bool IsNotIn<T>(this T? value, params T?[] values)
+        {
+            return !value.IsIn(equalityComparer: null, values);
+        }
+
+#if NET9_0_OR_GREATER
+        /// <summary>
+        /// Determines whether a specified collection does not contain the current value by using the default equality comparer.
+        /// </summary>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
+        /// <param name="value">The value to locate in the collection.</param>
+        /// <param name="equalityComparer">An equality comparer to compare values.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
+        /// <returns>Returns true if the specified collection does not contain an element that has the specified value; otherwise, false.</returns>
+        public static bool IsNotIn<T>(this T? value, IEqualityComparer<T?>? equalityComparer, params IEnumerable<T?> values)
+        {
+            return !value.IsIn(equalityComparer, values);
+        }
+
+        /// <summary>
+        /// Determines whether a specified collection does not contain the current value by using a specified <see cref="IEqualityComparer{T}"/>.
+        /// If no equality comparer is specified then the default equality comparer will be used.
+        /// </summary>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
+        /// <param name="value">The value to locate in the collection.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
+        /// <returns>Returns true if the specified collection does not contain an element that has the specified value; otherwise, false.</returns>
+        public static bool IsNotIn<T>(this T? value, params IEnumerable<T?> values)
+        {
+            return !value.IsIn(equalityComparer: null, values);
+        }
+#endif
+
+#if NET10_0_OR_GREATER
+        /// <summary>
+        /// Determines whether a specified collection does not contain the current value by using the default equality comparer.
+        /// </summary>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
+        /// <param name="value">The value to locate in the collection.</param>
+        /// <param name="equalityComparer">An equality comparer to compare values.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
+        /// <returns>Returns true if the specified collection does not contain an element that has the specified value; otherwise, false.</returns>
+        public static bool IsNotIn<T>(this T value, IEqualityComparer<T>? equalityComparer, params ReadOnlySpan<T> values)
+            where T : IEquatable<T>
+        {
+            return !value.IsIn(equalityComparer, values);
+        }
+
+        /// <summary>
+        /// Determines whether a specified collection does not contain the current value by using a specified <see cref="IEqualityComparer{T}"/>.
+        /// If no equality comparer is specified then the default equality comparer will be used.
+        /// </summary>
+        /// <typeparam name="T">The type of both the current value and the elements in <paramref name="values"/>.</typeparam>
+        /// <param name="value">The value to locate in the collection.</param>
+        /// <param name="values">A collection in which to locate a value.</param>
+        /// <returns>Returns true if the specified collection does not contain an element that has the specified value; otherwise, false.</returns>
+        public static bool IsNotIn<T>(this T value, params ReadOnlySpan<T> values)
+            where T : IEquatable<T>
+        {
+            return !value.IsIn(equalityComparer: null, values);
+        }
+#endif
     }
 }
