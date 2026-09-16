1924eca483242c4b2e02c1b635825178dc35395a
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2026-09-17 00:14:30 +0200

    Replace EncodingProvider with Encodings and a new extension property.

---
+++ src/EgonsoftHU.Extensions.Bcl/System/Text/EncodingExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright © 2022-2026 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+#if NET10_0_OR_GREATER
+using System.Text;
+
+namespace EgonsoftHU.Extensions.Bcl
+{
+    /// <summary>
+    /// <see cref="Encoding"/> extensions, e.g. extension property that provides UTF-8 encoding without the Unicode byte order mark.
+    /// </summary>
+    public static class EncodingExtensions
+    {
+        extension(Encoding encoding)
+        {
+            /// <summary>
+            /// The UTF-8 encoding without the Unicode byte order mark.
+            /// </summary>
+            /// <remarks>
+            /// <see cref="UTF8Encoding.GetPreamble"/> will return an empty byte array.
+            /// </remarks>
+            public static Encoding UTF8WithoutBOM => Encodings.LazyUTF8EncodingWithoutBOM.Value;
+        }
+    }
+}
+#endif
---
+++ src/EgonsoftHU.Extensions.Bcl/System/Text/Encodings.cs
@@ -0,0 +1,28 @@
+﻿// Copyright © 2022-2026 Gabor Csizmadia
+// This code is licensed under MIT license (see LICENSE for details)
+
+using System;
+using System.Text;
+
+namespace EgonsoftHU.Extensions.Bcl
+{
+    /// <summary>
+    /// An encoding provider, which supplies UTF-8 encoding without the Unicode byte order mark.
+    /// </summary>
+    public static class Encodings
+    {
+        internal static readonly Lazy<Encoding> LazyUTF8EncodingWithoutBOM =
+            new(() => new UTF8Encoding(encoderShouldEmitUTF8Identifier: false));
+
+        /// <summary>
+        /// The UTF-8 encoding without the Unicode byte order mark.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="UTF8Encoding.GetPreamble"/> will return an empty byte array.
+        /// </remarks>
+#if NET10_0_OR_GREATER
+        [Obsolete("Use System.Text.Encoding.UTF8WithoutBOM extension property instead.")]
+#endif
+        public static Encoding UTF8WithoutBOM => LazyUTF8EncodingWithoutBOM.Value;
+    }
+}
