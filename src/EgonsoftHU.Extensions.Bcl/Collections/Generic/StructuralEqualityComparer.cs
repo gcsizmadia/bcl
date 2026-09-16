7f200e07785d1c69563b017b029a3666e6e19068
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2026-09-16 23:46:12 +0200

    Update C# file headers.

--- src/EgonsoftHU.Extensions.Bcl/Collections/Generic/StructuralEqualityComparer.cs
+++ src/EgonsoftHU.Extensions.Bcl/Collections/Generic/StructuralEqualityComparer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2022-2024 Gabor Csizmadia
+﻿// Copyright © 2022-2026 Gabor Csizmadia
 // This code is licensed under MIT license (see LICENSE for details)
 
 using System.Collections;
