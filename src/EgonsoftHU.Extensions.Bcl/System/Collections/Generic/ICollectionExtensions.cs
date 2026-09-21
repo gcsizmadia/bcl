c2ce1c80dda729f4d482e295c087e81438a44a0d
Gabor Csizmadia <gcsizmadia@users.noreply.github.com>
2026-09-22 00:39:52 +0200

    Update ICollectionExtensions so that AddRange() extension method accepts both IEnumerable<T> and ReadOnlySpan<T> parameter types and also add params keyword in case of .NET 9 or later.

--- src/EgonsoftHU.Extensions.Bcl/System/Collections/Generic/ICollectionExtensions.cs
+++ src/EgonsoftHU.Extensions.Bcl/System/Collections/Generic/ICollectionExtensions.cs
@@ -25,7 +25,11 @@ namespace EgonsoftHU.Extensions.Bcl
         /// </param>
         /// <exception cref="ArgumentNullException">Either <paramref name="collection"/> or <paramref name="items"/> is <see langword="null"/>.</exception>
         /// <exception cref="ArgumentException"><paramref name="collection"/> is read-only.</exception>
+#if NET9_0_OR_GREATER
+        public static void AddRange<T>(this ICollection<T> collection, params IEnumerable<T> items)
+#else
         public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+#endif
         {
             collection.ThrowIfNull();
             items.ThrowIfNull();
@@ -41,10 +45,54 @@ namespace EgonsoftHU.Extensions.Bcl
                 return;
             }
 
-            foreach (T? item in items)
+            foreach (T item in items)
             {
                 collection.Add(item);
             }
         }
+
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+        /// <summary>
+        /// Adds the elements of the specified collection to the end of the current <paramref name="collection"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="collection">The current collection to the end of which the elements of the specified collection will be added.</param>
+        /// <param name="items">
+        /// The collection whose elements should be added to the end of the current <paramref name="collection"/>.
+        /// The collection itself cannot be <see langword="null" />,
+        /// but it can contain elements that are <see langword="null" />, if type <typeparamref name="T"/> is a nullable reference/value type.
+        /// </param>
+        /// <exception cref="ArgumentNullException">Either <paramref name="collection"/> or <paramref name="items"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="collection"/> is read-only.</exception>
+#if NET9_0_OR_GREATER
+        public static void AddRange<T>(this ICollection<T> collection, params ReadOnlySpan<T> items)
+#elif NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+        public static void AddRange<T>(this ICollection<T> collection, ReadOnlySpan<T> items)
+#endif
+        {
+            collection.ThrowIfNull();
+
+            if (collection.IsReadOnly)
+            {
+                throw ArgumentExceptions.CollectionIsReadOnly(nameof(collection));
+            }
+
+            if (items.IsEmpty)
+            {
+                return;
+            }
+
+            if (collection is List<T> list)
+            {
+                list.AddRange(items);
+                return;
+            }
+
+            foreach (T item in items)
+            {
+                collection.Add(item);
+            }
+        }
+#endif
     }
 }
